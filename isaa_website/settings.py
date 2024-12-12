4bd15e68ba184e7f5ed3c18383abd39c8e4d5d19
sahko123 <36277860+sahko123@users.noreply.github.com>
2024-12-12 21:43:55 +0000

    kys again

--- isaa_website/settings.py
+++ isaa_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-py6)n20jkl5b_0@zat^1m76xdz)%+#6hc=&lt43@dbgi(=v8ok'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ "*" ]
 
