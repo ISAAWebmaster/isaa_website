8c5716edc9e050fb68b7998ca794a114544f4f9d
sahko123 <36277860+sahko123@users.noreply.github.com>
2024-12-12 20:46:08 +0000

    fuck django sideways

--- isaa_website/settings.py
+++ isaa_website/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = BASE_DIR / 'productionfiles'
 STATIC_URL = 'static/'
 
 #Add this in your settings.py file:
-#STATICFILES_DIRS = [ BASE_DIR / 'staticfiles', ]
+STATICFILES_DIRS = [ BASE_DIR / 'staticfiles', ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
